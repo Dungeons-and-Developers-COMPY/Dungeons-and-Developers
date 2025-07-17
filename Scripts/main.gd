3e48e9ffc707b8152b009fe1297f7c525a6a8593
Mahir Moodaley <mahir12moodaley@gmail.com>
2025-07-17 11:47:12 +0200

    Player stops when they hit a monster

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -13,9 +13,9 @@ func _ready() -> void:
 	Globals.offset = maze.position
 	Globals.maze_scale = maze.scale.x
 	
-	player.set_maze_scale(maze.scale.x)
+	#player.set_maze_scale(maze.scale.x)
 	player.set_pos(maze.walls.start_coord["row"], maze.walls.start_coord["col"])
-	player.set_offset(maze.position)
+	#player.set_offset(maze.position)
 	player.set_grid(maze.walls.grid)
 	player.teleport()
 	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,11 +2,12 @@ extends Node2D
 class_name Player
 
 var pos: Vector2i = Vector2i(0, 0)
-var maze_scale = 1
-var pixels = 16
-var offset: Vector2 = Vector2(0, 0)
+#var maze_scale = 1
+#var pixels = 16
+#var offset: Vector2 = Vector2(0, 0)
 var grid = []
 var valid_funcs = ["move_up", "move_down", "move_left", "move_right"]
+var should_stop: bool = false
 
 #const HEADER = "extends Player\nfunc update_grid(new_grid): grid = new_grid\nfunc execute():\n"
 
@@ -15,24 +16,32 @@ var valid_funcs = ["move_up", "move_down", "move_left", "move_right"]
 # functions to move that player can type
 func move_left(n: int = 1):
 	for i in range(n):
+		if should_stop:
+			break
 		await try_move(pos.x - 1, pos.y)
 	
 func move_right(n: int = 1):
 	for i in range(n):
+		if should_stop:
+			break
 		await try_move(pos.x + 1, pos.y)
 	
 func move_up(n: int = 1):
 	for i in range(n):
+		if should_stop:
+			break
 		await try_move(pos.x, pos.y - 1)
 	
 func move_down(n: int = 1):
 	for i in range(n):
+		if should_stop:
+			break
 		await try_move(pos.x, pos.y + 1)
 
 # function to move player to the grid coordinate
 func move():
-	var x = (pos.x * maze_scale * pixels) + (offset.x + pixels)
-	var y = (pos.y * maze_scale * pixels) + (offset.y + pixels)
+	var x = (pos.x * Globals.maze_scale * Globals.pixels) + (Globals.offset.x + Globals.pixels)
+	var y = (pos.y * Globals.maze_scale * Globals.pixels) + (Globals.offset.y + Globals.pixels)
 	char.set_target_pos(x, y)
 	await char.stopped_moving
 
@@ -41,6 +50,8 @@ func try_move(x: int, y: int):
 	if (can_move(x, y)):
 		pos.x = x
 		pos.y = y
+		if (on_monster_coord()):
+			should_stop = true
 		await move()
 	# TODO: else stun player
 
@@ -57,8 +68,8 @@ func can_move(x: int, y: int):
 
 # function to move player to its current position without movement 
 func teleport():
-	var x_pos = (pos.x * maze_scale * pixels) + (offset.x + pixels)
-	var y_pos = (pos.y * maze_scale * pixels) + (offset.y + pixels)
+	var x_pos = (pos.x * Globals.maze_scale * Globals.pixels) + (Globals.offset.x + Globals.pixels)
+	var y_pos = (pos.y * Globals.maze_scale * Globals.pixels) + (Globals.offset.y + Globals.pixels)
 	char.global_position = Vector2(x_pos, y_pos)
 
 # set fuctions for variables
@@ -66,11 +77,11 @@ func set_pos(x: int, y: int):
 	pos.x = x
 	pos.y = y
 	
-func set_offset(pos: Vector2):
-	offset = pos
-	
-func set_maze_scale(s):
-	maze_scale = s
+#func set_offset(pos: Vector2):
+	#offset = pos
+	#
+#func set_maze_scale(s):
+	#maze_scale = s
 	
 func set_grid(g):
 	grid = g
@@ -80,6 +91,9 @@ func execute_move(code: String):
 	var lines = code.split("\n")
 	
 	for line in lines:
+		if should_stop:
+			break
+			
 		line = line.strip_edges()
 		if line == "":
 			continue
